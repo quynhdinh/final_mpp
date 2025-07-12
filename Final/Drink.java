4e73732f10fa9f1c20c2c09af129e07200508300
quynhdv <quynhvdinh@gmail.com>
2025-07-12 11:53:23 -0500

    improve Drink

--- Final/Drink.java
+++ Final/Drink.java
@@ -1,14 +1,15 @@
 package Final;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class Drink {
-    private static Double parseString(String x){
+    private static Optional<Integer> parseString(String x){
         try {
-            double double1 = Double.parseDouble(x);
-            return double1;
+            int i = Integer.parseInt(x);
+            return Optional.of(i);
         } catch (Exception e) {
-            return null;
+            return Optional.empty();
         }
     }
     public static void main(String[] args) {
@@ -16,11 +17,13 @@ public class Drink {
             Map.entry("Cold Brew Coffee", "0"),
             Map.entry("Iced Green Tea", "12"),
             Map.entry("Sparkling Water", "N/A"),
+            Map.entry("Lemonade", "28"),
+            Map.entry("Berry Smoothie", "N/A"),
             Map.entry("Orange Juice", "23")
         );
-        m.entrySet().stream().filter(e -> (parseString(e.getValue()) != null))
+        m.entrySet().stream().filter(e -> (parseString(e.getValue()).isPresent()))
         .filter(e -> e.getKey().toLowerCase().contains("juice") || e.getKey().equalsIgnoreCase("smoothie"))
-        .filter(e -> (parseString(e.getValue()) < 30))
+        .filter(e -> (parseString(e.getValue()).get() < 30))
         .sorted(Map.Entry.comparingByValue())
         .sorted(Map.Entry.comparingByKey())
         .forEach(e -> System.out.println(String.format(e.getKey() + ": " + e.getValue())));
